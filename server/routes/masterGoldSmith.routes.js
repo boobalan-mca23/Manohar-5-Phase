f2b093fb3bb7deb92ed6b25939208a41dd6b19fe
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-12-03 12:59:06 +0530

    Update masterGoldSmith.routes.js

--- server/routes/masterGoldSmith.routes.js
+++ server/routes/masterGoldSmith.routes.js
@@ -7,5 +7,6 @@ router.post('/',goldSmith.createGoldSmith)
 router.put('/:id',goldSmith.updateGoldSmith)
 router.delete('/:id',goldSmith.deleteGoldSmithById)
 router.get('/',goldSmith.getAllGoldSmith)
+router.get('/allGS', goldSmith.getAllGS)
 
 module.exports=router
