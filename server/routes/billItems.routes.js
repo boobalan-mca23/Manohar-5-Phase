9a59b717e90d605ece3df15a458fc3fe4f5cf2d5
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-12-05 15:08:51 +0530

    Delete server/routes directory

--- server/routes/billItems.routes.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const { getBillItems, createBillItems, getItemBySerialNumber } = require('../controllers/billItems.controllers');
-// const router = express.Router()
-
-// router.get('/getAll',getBillItems)
-// router.get('/getSerial/:id',getItemBySerialNumber)
-// router.post('/create',createBillItems)
-
-// module.exports = router;  
