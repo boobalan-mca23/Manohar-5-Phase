6f3c0cf5c20ed203e464589d25cc61714bd086a4
Guru <139735814+Gurunath-S@users.noreply.github.com>
2025-12-09 12:00:44 +0530

    Update RemovedLots.jsx

--- client/src/Components/RemovedLots/RemovedLots.jsx
+++ client/src/Components/RemovedLots/RemovedLots.jsx
@@ -1,213 +1,196 @@
-import Table from "react-bootstrap/esm/Table";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import SearchIcon from "@mui/icons-material/Search";
-import { TextField, InputAdornment } from "@mui/material";
-import RestorePageIcon from "@mui/icons-material/RestorePage";
-import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import Checkbox from "@mui/material/Checkbox";
+import { useState, useEffect, useCallback} from "react";
+import RemovedLotTable from "./RemovedLotTable";
+import { REACT_APP_BACKEND_SERVER_URL } from "../../config";
+import axios from "axios";
 import "./RemovedLots.css";
-import React from "react";
-import { useState } from "react";
-import AlertDialog from "./DialogBox";
-import CircularProgress from "@mui/material/CircularProgress";
+import { Snackbar, Alert } from "@mui/material";
 
-const RemovedLotTable = (props) => {
-  const [open, setOpen] = useState(false);
-  const [confirmMessage, setConfirmMessage] = useState("");
-  const [access, setAccess] = useState("");
-  
-  const { removedLots, handleDelete, handleRestore ,loading,selectedProduct,setSelectedProduct, page, totalPage, setPage } = props;
 
-  const handleSelect = (id, checked) => {
-  setSelectedProduct((prev) => {
-    let updatedList = [...prev.selectedItems];
-
-    if (checked) {
-      // Add id if not exist
-      if (!updatedList.includes(id)) {
-        updatedList.push(id);
-      }
-    } else {
-      // Remove when unchecked
-      updatedList = updatedList.filter((item) => item !== id);
-    }
-
-    return {
-      count: updatedList.length,
-      selectedItems: updatedList,
-    };
+const RemoveLot = () => {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(8);
+  const [totalPage, setTotalPage] = useState(1);
+  const [removedLots, setRemovedLots] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [search, setSearch] = useState("");  // actual debounced value
+  const [snackbar, setSnackbar] = useState({
+    open: false,
+    message: "",
+    severity: "",
   });
-};
+  const [selectedProduct,setSelectedProduct]=useState({
+      count:0,
+      selectedItems:[]
+  })
 
-const handleSelectAll = (checked) => {
-  if (checked) {
-    const allIds = removedLots.map((item) => item.id);
-    setSelectedProduct({
-      count: allIds.length,
-      selectedItems: allIds,
-    });
-  } else {
-    setSelectedProduct({
-      count: 0,
-      selectedItems: [],
-    });
-  }
-};
-
-
-  const handleConfirm = () => {
-    access === "restore" ? handleRestore() : handleDelete();
+  const handleCloseSnackbar = () => {
+    setSnackbar({ ...snackbar, open: false });
   };
 
-  const handleRestoreLot = () => {
-     if(selectedProduct.selectedItems.length!==0){
-         setOpen(true);
-         setConfirmMessage(`Are You Want To Restore The Selected Lots LOT ID: ${selectedProduct.selectedItems}`);
-         setAccess("restore");
-     }
-   
-   
+
+  useEffect(() => {
+  const delay = setTimeout(() => {
+    setSearch(searchInput);  // update real search only after 300ms
+  }, 300);
+
+  return () => clearTimeout(delay);
+}, [searchInput]);
+
+  const fetchRemovedLots = useCallback(async () => {
+    try {
+      
+      const response = await axios.get(
+        `${REACT_APP_BACKEND_SERVER_URL}/api/v1/restoreLot/getDiactivateLots`,
+        { params: { page, limit ,search} }
+      );
+
+      setRemovedLots(response.data.deletedLots);
+      setTotalPage(response.data.totalPage ? response.data.totalPage:1  );
+     
+      setTimeout(() => setLoading(false), 1000);
+      
+
+    } catch (err) {
+      console.log("err", err.message);
+      setSnackbar({
+        open: true,
+        message: "Failed to Fetch Removed Lots",
+        severity: "error",
+      });
+       setLoading(false);
+    }
+  }, [page, limit,search]);
+
+
+  const handleRestore = async () => {
+    try {
+      const payload={
+         selectedProduct:selectedProduct.selectedItems
+      }
+      const resposne = await axios.put(
+        `${REACT_APP_BACKEND_SERVER_URL}/api/v1/restoreLot/changetoActivate`,
+      payload
+           
+      );
+      if (resposne.data.success === true) {
+        setSnackbar({
+          open: true,
+          message: resposne.data.message,
+          severity: "success",
+        });
+      const updatedRemovedLots = removedLots.filter(
+         (item) => !selectedProduct.selectedItems.includes(item.id)
+        );
+        
+        setRemovedLots(updatedRemovedLots);
+         
+        // also clear selected items
+      
+         setSelectedProduct({
+        count: 0,
+        selectedItems: []
+       });
+      }
+    } catch (err) {
+      console.log("err", err.message);
+      setSnackbar({
+        open: true,
+        message: "Error on Restore Lot",
+        severity: "error",
+      });
+    }
   };
 
-  const handleDeleteLot = () => {
-     if(selectedProduct.selectedItems.length!==0){
-      setOpen(true);
-      setConfirmMessage(
-      `Are you sure you want to permanently delete this lot? All products under this lot will also be removed.
-    LOT ID:  ${selectedProduct.selectedItems}`);
-      setAccess("delete");
-     }
-   
+  const handleDelete = async () => {
+    try {
+      const payload={
+         selectedProduct:selectedProduct.selectedItems
+      }
+
+      const resposne = await axios.put(
+        `${REACT_APP_BACKEND_SERVER_URL}/api/v1/lot/lotDelete`,
+        payload
+      );
+
+      if (resposne.data.success === true) {
+        setSnackbar({
+          open: true,
+          message: resposne.data.message,
+          severity: "success",
+        });
+       const updatedRemovedLots = removedLots.filter(
+         (item) => !selectedProduct.selectedItems.includes(item.id)
+        );
+        setRemovedLots(updatedRemovedLots);
+         
+        // also clear selected items
+      
+         setSelectedProduct({
+        count: 0,
+        selectedItems: []
+       });
+
+        
+      }
+    } catch (err) {
+      console.log("err", err.message);
+      setSnackbar({
+        open: true,
+        message: "Error on Delete Lot",
+        severity: "error",
+      });
+    }
   };
+
+  useEffect(() => {
+    fetchRemovedLots();
+  }, [fetchRemovedLots]);
 
   return (
     <>
-      <div className="rl-table-wrapper">
-    <div className="rl-table-header">
-            <h2>Removed Lots Information</h2>
-            <FontAwesomeIcon icon={faTrashAlt} fontSize={20} />
-          </div>
-      
-         <div className="rl-action-buttons">
-          <TextField
-            autoComplete="off"
-            placeholder="Search By Lot Id,Type etc.."
-            variant="outlined"
-            margin="normal"
-            fullWidth={false}
-            className="rl-search"
-            onChange={(e)=>{ props.setSearchInput(e.target.value)}}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="end">
-                  <SearchIcon style={{ color: "#373333ff" }} />
-                </InputAdornment>
-              ),
-            }}
-          />
-          <button
-            className="rl-action-button-1"
-            onClick={()=>{handleRestoreLot()}}>
-            <RestorePageIcon />{`Restore Selected [${selectedProduct.count}]`}
-          </button>
-          <button
-            className="rl-action-button-2"
-            onClick={()=>{handleDeleteLot()}}>
-            <DeleteForeverIcon /> {`Permantely Delete Selected [${selectedProduct.count}]`}
-          </button>
-          </div>
-
-
-        <Table striped bordered hover className="rl-table">
-          <thead>
-            <tr>
-              <th>S.No</th>
-              <th>Date</th>
-              <th>Lot Name</th>
-              <th>Lot Type</th>
-              <th><Checkbox
-                  style={{
-                    color:"whitesmoke"
-                  }}
-                  checked={selectedProduct.selectedItems.length === removedLots.length}
-                  onChange={(e) => handleSelectAll(e.target.checked)}
-                  />
-               Select All
-             </th>
-            </tr>
-          </thead>
-          <tbody>
-            {loading ? (
-              // Show loader for 5 sec
-              <tr>
-                <td colSpan={5}>
-                  <div className="rl-loader"
-                  >
-                    <CircularProgress size="2rem" />
-                    <p>Loading...</p>
-                  </div>
-                </td>
-              </tr>
-            ) : removedLots.length >= 1 ? (
-              removedLots.map((item, index) => (
-                <tr key={item.id}>
-                  <td>{index + 1}</td>
-                  <td>
-                    {new Date(item.created_at)
-                      .toLocaleDateString("en-GB")
-                      .replace(/\//g, "-")}
-                  </td>
-                  <td>{item.lot_name}</td>
-                  <td
-                    style={{ color: item.type === "STONE" ? "green" : "blue" }}
-                  >
-                    <b>{item.type}</b>
-                  </td>
-                  <td> 
-                    <input
-                     type="checkbox"
-                     checked={selectedProduct.selectedItems.includes(item.id)}
-                     onChange={(e) => handleSelect(item.id, e.target.checked)}
-                     />
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td
-                  colSpan={5}
-                  className="rl-no-data"
-                >
-                  <b>No Lots Available</b>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
-        <div className="rl-nav-button" >
-          <button disabled={page === 1} onClick={() => setPage(page - 1)}>
-            ◀ Prev
-          </button>
+    <div className="rl-page">
+      {/* <div className="rl-card"> */}
+      <RemovedLotTable
+        removedLots={removedLots}
+        handleDelete={handleDelete}
+        handleRestore={handleRestore}
+        loading={loading}
+        setSearchInput={setSearchInput} 
+        selectedProduct={selectedProduct}
+        setSelectedProduct={setSelectedProduct}
+        page={page}
+        totalPage={totalPage}
+        setPage={setPage}
+      />
   
-          <span>
-            Page {page} of {totalPage}
-          </span>
-  
-          <button disabled={page === totalPage} onClick={() => setPage(page + 1)}>
-            Next ▶
-          </button>
-        </div>
-        <AlertDialog
-          open={open}
-          setOpen={setOpen}
-          setAccess={setAccess}
-          confirmMessage={confirmMessage}
-          handleConfirm={handleConfirm}
-        />
+
+      {/* Snackbar for feedback */}
+      <Snackbar
+        open={snackbar.open}
+        autoHideDuration={4000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        sx={{ mt: 8, ml: 4 }}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbar.severity}
+          variant="filled"
+          style={{
+            backgroundColor:
+              snackbar.severity === "error" ? "#bc1823" : "#4BB543",
+            color: "#fff4dcc",
+            fontFamily: "'Poppins', sans-serif",
+          }}
+        >
+          {snackbar.message}
+        </Alert>
+      </Snackbar>
+      {/* </div> */}
       </div>
     </>
   );
 };
 
-export default React.memo(RemovedLotTable);
+export default RemoveLot;
