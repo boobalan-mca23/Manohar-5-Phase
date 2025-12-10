a52c8c20400fbd8ec9b2ffebc96f3ec19a58eb37
boobalan <boobalan-mca23>
2025-12-10 16:14:16 +0530

    fix correct folder

--- server/serialPort/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const exprees=require('express')
-const router=exprees.Router()
-
-const auth=require('../controllers/auth.controller')
-router.post('/register',auth.userRegister)
-router.post('/login',auth.loginUser)
-
-module.exports=router
--- server/serialPort/routes/image.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadImage, getImages } = require("../controllers/imageContoller");
-
-router.post("/upload", uploadImage);
-router.get("/", getImages);
-
-module.exports = router;
